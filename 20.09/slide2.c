2ae0b8582df4206a58ea9bcfd9955520b319b22b
Ssssskl <126408807+Ssssskl@users.noreply.github.com>
2023-09-20 01:23:17 +0300

    Update slide2.c

--- 20.09/slide2.c
+++ 20.09/slide2.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-void reverse(char s[])
+void reverse(char *s)
 {
     int m=strlen(s);
-    char l[m];
-    for (int i=m-1; i>=0; i--)
+    char c;
+    for (int i=0; i<m/2; i++)
     {
-        l[m-i-1]=s[i];
+        c=s[i];
+        s[i]=s[m-i-1];
+        s[m-i-1]=c;
     }
-    printf("%s\n", l);
+    printf("%s\n", s);
 } 
 
 int main()
 {
-    char s[]="dfghjkl";
+    char s[]="asdfg";
     reverse(s);
 }
