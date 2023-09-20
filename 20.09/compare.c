c5ba817b04e95126fbfd1b3b2fc1f9e8f6282346
Ssssskl <126408807+Ssssskl@users.noreply.github.com>
2023-09-20 11:04:25 +0300

    Update compare.c

--- 20.09/compare.c
+++ 20.09/compare.c
@@ -3,16 +3,19 @@
 
 int mystrcmp(char *s1, char *s2)
 {
-    for (*s1,*s2; (*s1==*s2) && (*s1!=0); s1++, s2++);
-    if (*s1!=*s2) 
-        return 0;
-    else
-        return 1;
+    for (*s1,*s2; (*s1==*s2) && (*s1!=0) && (*s2!=0); s1++, s2++);
+        if (*s1<*s2) 
+            return -1;
+        else
+            if (*s1>*s2)
+                return 1;
+            else
+                return 0; 
 }
 
 int main()
 {
     char s1[]="asdfg";
-    char s2[]="asdfgee";
+    char s2[]="asdfg";
     printf("%d\n", mystrcmp(s1,s2));
 }
