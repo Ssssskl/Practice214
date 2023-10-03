c0d28b15729ee74e8c1548d3aab566c150e63a80
Ssssskl <126408807+Ssssskl@users.noreply.github.com>
2023-10-04 01:33:39 +0300

    Create func_list.c

---
+++ 04.10/func_list.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+Описать структуру однонаправленного списка целых чисел,
+-- написать функцию,
+      --- вставляющую заданный элемент в конец заданного списка,
+      ---  удаляющую первое вхождение заданного элемента из заданного списка,
+      ---  удаляющую все вхождения заданного элемента из заданного списка.
+*/
+struct list
+{
+    int value;
+    struct list *next;
+};
+
+void toend (struct list *next, int elem)
+{
+    struct list *m;
+    m = next;
+    while (m -> next) 
+    {
+        m = m -> next;
+    }
+    list *t = (struct list*)malloc(sizeof(struct list));
+    t -> value = elem;
+    t -> next = NULL;
+    m -> next = t;
+}
+
+void del_1 (struct list *next, int elem)
+{
+    struct list *p, *m;
+    p = NULL;
+    m = next;
+    while (m != NULL)
+    {
+        if ( m -> value = elem)
+        {
+            p -> next = m -> next;
+            free(m);
+            break;
+        }
+        p = m;
+        m = m -> next;
+    }
+}
+
+void del_all(struct list *next, int elem)
+{
+    struct list *p, *m;
+    p = NULL;
+    m = next;
+    while (m != NULL)
+    {
+        if ( m -> value = elem)
+        {
+            p -> next = m -> next;
+            free(m);
+        }
+        p = m;
+        m = m -> next;
+    }
+}
