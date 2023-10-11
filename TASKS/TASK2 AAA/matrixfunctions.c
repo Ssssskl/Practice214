c5150a74612d0bf2303a79e8706bf2a5e790a048
Ssssskl <olyaf12309@gmail.com>
2023-10-11 12:10:14 +0300

    matrix = NULL check

--- TASKS/TASK2 AAA/matrixfunctions.c
+++ TASKS/TASK2 AAA/matrixfunctions.c
@@ -39,7 +39,7 @@ long double **read_matrix (unsigned long long *line, unsigned long long *column)
 
 void out_matrix(long double **matrix, unsigned long long line, unsigned long long column)
 {
-    if (matrix == NULL)
+    if (!matrix)
         return;
     printf("Your matrix:\n");
     for (unsigned int i = 0; i < line; i++)
@@ -62,6 +62,7 @@ void matrix_free(long double **matrix, unsigned long long line)
     free(matrix);
 }
 
+
 long double **sum_two_matrix (long double **matrix_1, unsigned long long *line_1, unsigned long long *column_1, long double **matrix_2, unsigned long long *line_2, unsigned long long *column_2)
 {
     if (!matrix_1  || !matrix_2)
@@ -69,15 +70,15 @@ long double **sum_two_matrix (long double **matrix_1, unsigned long long *line_1
         printf("ERROR! That's not a matrix!\n");
         return NULL;
     }
-    if (*line_1 != *line_2 && *column_1 != *column_2)
+    if ( (*line_1 != *line_2) || (*column_1 != *column_2) )
     {
         printf("ERROR! The matrices have different sizes\n");
         return NULL;
     }
-    long double **result = malloc(*line_1*sizeof(long double*));
+    long double **result = calloc(*line_1, sizeof(long double*));
     for (unsigned int i = 0; i < *line_1; i++)
     {
-        result[i] = malloc(*column_1*sizeof(long double));
+        result[i] = calloc(*column_1, sizeof(long double));
         for (unsigned int j = 0; j < *column_1; j++)
         {
             result[i][j] =  matrix_1[i][j] + matrix_2[i][j];
@@ -93,10 +94,10 @@ long double **mul_on_num (long double **matrix, unsigned long long *line, unsign
         printf("ERROR! That's not a matrix!\n");
         return NULL;
     }
-    long double **result = malloc(*line*sizeof(long double*));
+    long double **result = calloc(*line, sizeof(long double*));
     for (unsigned int i = 0; i < *line; i++)
     {
-        result[i] = malloc(*column*sizeof(long double));
+        result[i] = calloc(*column, sizeof(long double));
         for (unsigned int j = 0; j < *column; j++)
         {
             result[i][j] =  matrix[i][j] * num;
@@ -114,13 +115,13 @@ long double **mul_two_matrix (long double **matrix_1, unsigned long long *line_1
     }
     if (*column_1 != *line_2)
     {
-        printf("ERROR! The matrices have incorrect sizes for multiplication");
+        printf("ERROR! The matrices have incorrect sizes for multiplication\n");
         return NULL;
     }
-    long double **result = malloc(*line_1*sizeof(long double*));
+    long double **result = calloc(*line_1, sizeof(long double*));
     for (unsigned int i = 0; i < *line_1; i++)
     {
-        result[i] = malloc(*column_2*sizeof(long double));
+        result[i] = calloc(*column_2, sizeof(long double));
         for (unsigned int j = 0; j < *column_2; j++)
         {
             result[i][j] = 0;
