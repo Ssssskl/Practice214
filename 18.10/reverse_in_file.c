ca6c1ee6cd1bd09ef6e99d33e6b97a27bb4b6b69
Ssssskl <olyaf12309@gmail.com>
2023-10-25 17:16:01 +0300

    push hw

---
+++ 18.10/reverse_in_file.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack{
+    int i;
+    struct stack *next;
+} stack;
+
+void put_in(stack ** st, int i)
+{
+    stack *st1;
+    st1 = malloc(sizeof(stack));
+    st1 -> i = i;
+    st1 -> next = *st;
+    (*st) = st1;
+}
+
+void print_Stack_in_file(stack *st, FILE *f)
+{
+    while(st != NULL)
+    {
+        fprintf(f, "%d", st -> i);
+        st = st -> next;
+    }
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *f = fopen("./file.txt", "r");
+    stack *st =NULL;
+    int n;
+    while(fscanf(f, "%d", &n) != EOF)
+    {
+        put_in(&st, n);
+    }
+    f = fopen("./file.txt", "w");
+    print_Stack_in_file(st, f);
+    fclose(f);
+    return 0;
+}
